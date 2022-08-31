4456b5d68cf50b3fb61e6c72d19b82e0e1ce57f2
Enzo Giovannetti <97138287+enzoGiovannetti@users.noreply.github.com>
2022-08-31 18:34:23 -0300

    Create funcoes.js

---
+++ aprendendo_JS/funcoes.js
@@ -0,0 +1,18 @@
+//let x = ''
+//console.log(x)
+//x = 'oi'
+//console.log(x)
+
+function imprimeTexto(texto){
+  console.log(texto)
+}
+
+
+imprimeTexto(soma())
+
+
+//tres formas de escrever funções
+
+function soma() {
+  return 2 + 2
+}
