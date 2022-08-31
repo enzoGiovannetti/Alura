cb6a1e3f3d8fcbd8218e168f221bbef09b48bfd4
Enzo Giovannetti <97138287+enzoGiovannetti@users.noreply.github.com>
2022-08-31 17:40:44 -0300

    Create template_string.js

---
+++ aprendendo_JS/template_string.js
@@ -0,0 +1,9 @@
+const nome = 'enzo'
+const idade = 2022-2002
+const cidade = 'Sampa meuuuu'
+//const apresentacao = 'meu nome é ' + nome
+
+const apresentacao = 'meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade} '
+
+console.log(apresentacao)
+
