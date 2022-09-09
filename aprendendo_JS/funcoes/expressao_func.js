5aef5e2e78bea36da5b58ba0bb66d71ad5868b23
Enzo Giovannetti <97138287+enzoGiovannetti@users.noreply.github.com>
2022-09-09 16:15:54 -0300

    expressão de função

---
+++ aprendendo_JS/funcoes/expressao_func.js
@@ -0,0 +1,13 @@
+// expressao de função
+
+const soma = function(num1,num2) {return num1+num2}
+
+//console.log(soma(11,11))
+
+//diferença principal entre expressão e declaração
+
+console.log(apresentar())
+
+function apresentar(){
+  return 'ola'
+}
