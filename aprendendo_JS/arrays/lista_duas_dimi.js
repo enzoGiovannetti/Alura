c2bf09a5430b275d5e49f59d3b57f8882221c6a3
Enzo Giovannetti <97138287+enzoGiovannetti@users.noreply.github.com>
2022-09-22 17:44:11 -0300

    Create lista_duas_dimi.js

---
+++ aprendendo_JS/arrays/lista_duas_dimi.js
@@ -0,0 +1,7 @@
+const alunos = ['Jao', 'xuliana', 'caio', 'ana']
+const medias_dos_aluno = [10,7,9,6]
+
+let lista_alunos_notas = [alunos,medias_dos_aluno]
+console.log(lista_alunos_notas)
+
+console.log(lista_alunos_notas[0][0] +', sua nota é '+lista_alunos_notas[1][0])
