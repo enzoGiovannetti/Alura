5017eb202be513293ed008f5c913f01948006987
Enzo Giovannetti <97138287+enzoGiovannetti@users.noreply.github.com>
2022-09-22 17:32:27 -0300

    Create concat.js

---
+++ aprendendo_JS/arrays/concat.js
@@ -0,0 +1,6 @@
+const sala_de_python = ['Melissa', 'Malena', 'Rods']
+const sala_js = ['ju', 'leo', 'Rachel']
+
+const duas_juntas = sala_de_python.concat(sala_js)
+
+console.log('Duas salas juntas: ' + duas_juntas)
